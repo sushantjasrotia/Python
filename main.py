a586f29af14835eabcc68c1e974e4095c5aa02c8
sushantjasrotia <42170316+sushantjasrotia@users.noreply.github.com>
2023-10-09 22:11:12 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,46 +1,52 @@
-from turtle import Screen
-from snake import Snake
-from food import Food
+from turtle import Screen, Turtle
+from paddle import Paddle
+from ball import Ball
 from scoreboard import Scoreboard
 import time
 
 screen = Screen()
-screen.setup(width=600, height=600)
-screen.bgcolor("black")  # screen background color
-screen.title("My Snake Game")
-screen.tracer(0)
+screen.bgcolor("black")
+screen.setup(width= 800, height=600)
+screen.title("Pong")
+screen.tracer(0) # turn off the animation
 
-snake = Snake()
-food = Food()
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
+Ball = Ball()
 scoreboard = Scoreboard()
+
+
 screen.listen()
-screen.onkey(snake.up, "Up")
-screen.onkey(snake.down, "Down")
-screen.onkey(snake.left, "Left")
-screen.onkey(snake.right, "Right")
+screen.onkey(r_paddle.go_up, "Up")
+screen.onkey(r_paddle.go_down, "Down")
+screen.onkey(l_paddle.go_up, "w")
+screen.onkey(l_paddle.go_down, "s")
 
-game_is_on = True
+
+game_is_on = True  #due to tracer function we have to update screen manually
 while game_is_on:
+    time.sleep(Ball.move_speed)
     screen.update()
-    time.sleep(0.1)
-    snake.move()
+    Ball.move()
 
-    #DETECT collison with food
-    if snake.head.distance(food) < 15: #15 is pixle
-        food.refresh()
-        snake.extend()
-        scoreboard.increase_score()
+    #detecting collision with ball
+    if Ball.ycor() > 280 or Ball.ycor() < -280:
+        Ball.bounce_y()
 
-    #Detect collison with wall.
-    if snake.head.xcor() > 280 or snake.head.xcor() <-280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
-        game_is_on = False
-        scoreboard.game_over()
+    #Detect collision with  paddles
+    if Ball.distance(r_paddle) < 50 and Ball.xcor() > 320 or Ball.distance(l_paddle) < 50 and Ball.xcor() < -320:
+        Ball.bounce_x()
 
-    #Detect collision with tail.
-    for segment in snake.segments[1:]: #list slice
-         if snake.head.distance(segment) < 10:
-            game_is_on = False
-            scoreboard.game_over()
+    #Detect when r_paddle miss the ball
+    if Ball.xcor() > 380:
+        Ball.reset_position()
+        scoreboard.l_point()
+
+    #detect l_paddle miss
+    if Ball.xcor() < -380:
+        Ball.reset_position()
+        scoreboard.r_point()
+
 
 
 
